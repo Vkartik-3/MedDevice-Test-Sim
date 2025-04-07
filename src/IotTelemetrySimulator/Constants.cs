11418f40f32bb726722a0a5bdcbc3765f333ae3e
Vkartik-3 <kartikvadhwana7@gmail.com>
2025-04-07 14:33:20 -0400

    Initial commit - custom version for Philips Embedded Testing

---
+++ src/IotTelemetrySimulator/Constants.cs
@@ -0,0 +1,27 @@
+﻿namespace IotTelemetrySimulator
+{
+    public static class Constants
+    {
+        public const string AppVersion = "1.1";
+
+        /// <summary>For backward compatibility.</summary>
+        public const string LegacyIntervalConfigName = "Interval";
+        public const string IntervalsConfigName = "Intervals";
+        public const string TemplateConfigName = "Template";
+        public const string PayloadDistributionConfigName = "PayloadDistribution";
+
+        public const string TimeValueName = "Time";
+        public const string LocalTimeValueName = "LocalTime";
+        public const string EpochValueName = "Epoch";
+        public const string TicksValueName = "Ticks";
+        public const string DeviceIdValueName = "DeviceId";
+        public const string GuidValueName = "Guid";
+        public const string MachineNameValueName = "MachineName";
+        public const string IterationNumberValueName = "IterationNumber";
+        public static readonly string[] AllSpecialValueNames =
+        {
+            TimeValueName, LocalTimeValueName, EpochValueName, TicksValueName,
+            DeviceIdValueName, GuidValueName, MachineNameValueName, IterationNumberValueName
+        };
+    }
+}
